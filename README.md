e67abab0f8c4f5894070c6c6afd1d4f9d556edfe
akshayblevel <akshayblevel@gmail.com>
2021-07-26 18:25:40 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-uiqxci
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-uiqxci)
---
+++ src/app.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Greeter(props:any){
+  return <div> Hello {props.name} with age {props.age}!</div>
+}
+
+function App() {
+  return (
+    <div>
+      <Greeter name="Akshay" age={39} />
+      <Greeter name="Panth" age ={4} />
+    </div>
+  );
+}
+
+export default App;
+
+
